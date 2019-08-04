466a1a2172014b9e3ef7014b4dd40e16494d713c
Chris Brown <chrisb@readinghorizons.com>
2019-08-03 21:50:33 -0600

    Init Bulma Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/main.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="hero is-primary">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
