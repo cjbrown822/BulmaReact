a16a91c183ce0e1510e4fee17f7e3cc3734d8fbe
Chris Brown <chrisb@readinghorizons.com>
2019-08-05 13:53:17 -0600

    update init

---
+++ src/components/higherorder/stylehoc.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const style = WrappedComponent => props => {
+	const { primary, danger, warning, success, info, ...rest} = props;
+	let forwardedStyle = "";
+	if(primary) forwardedStyle = "primary";
+	else if(danger) forwardedStyle = "danger";
+	else if(warning) forwardedStyle = "warning";
+	else if(success) forwardedStyle = "success";
+	else if(info) forwardedStyle = "info";
+	return (
+		<WrappedComponent styleName={forwardedStyle} {...rest} />
+	)
+};
